87bf375e4daf89513fc80ac7673bd0178aeafb50
iamShah <formal>
2020-11-22 18:48:03 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -7,19 +7,7 @@
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
-# NO, NOT SINCE 1938!
 
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
-# def speak_to_grandma(speak_to)
-#     if(speak_to!=upcase)
-#         puts "HUH?! SPEAK UP, SONNY!"
-#     elseif(speak_to!=downcase)
-#         puts "NO, NOT SINCE 1938!"
-#     else(speak_to==upcase)
-#         puts "I LOVE YOU TOO PUMPKIN!"
-#     end
-    
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
